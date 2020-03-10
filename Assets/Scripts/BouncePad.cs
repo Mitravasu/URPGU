0ff102d0c062638ab16912f9bb25b6292d8aaad0
Mitravasu Prakash <43656604+Mitravasu@users.noreply.github.com>
2020-03-10 15:38:43 -0400

    You have activated MU's Trap Card
    akfnaofnaofnoanf

---
+++ Assets/Scripts/BouncePad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    public int jumpForce = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Player") {
+            other.rigidbody.AddForce(new Vector3(0, jumpForce, 0));
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SpeedPowerUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Player") {
+            GameObject.Find("Player").GetComponent<PlayerMovement>().velocity *=2;
+            GameObject.Find("Player").GetComponent<PlayerMovement>().jump *=2;
+            Destroy(GameObject.Find("Speed"));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
