9e7ddd25210a3006f5f40ef14caa04a5a295482e
Mitravasu Prakash <43656604+Mitravasu@users.noreply.github.com>
2020-03-10 15:51:06 -0400

    Darty Boi MU
    adadadaf

---
+++ Assets/Scripts/Dart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision other) {
+    if(other.gameObject.tag == "Player") {
+        Instantiate(other.gameObject, other.transform.position + new Vector3(100, 100, 100), other.transform.rotation);
+    }
+    }
+
+    void Update()
+    {
+        
+    }
+}
