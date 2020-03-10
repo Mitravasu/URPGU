384dc047e286211b3dfcbb6fec25387327fed224
Colin Lin <colincool100@gmail.com>
2020-03-10 14:33:35 -0400

    raycast

---
+++ Assets/Scripts/raycast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class raycast : MonoBehaviour
+{
+    [SerializeField] private Material yellowMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit)){
+            var selection = hit.transform;
+            var selectionRender = selection.GetComponent<Renderer>();
+            if(selectionRender!=null){
+                selectionRender.material = yellowMaterial;
+            }
+        }
+        
+    }
+}
