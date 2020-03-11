35dc3de955a839307e53f9a30f35e7d2ef40e4fc
chewah <chewah@users.noreply.github.com>
2020-03-11 00:43:45 -0400

    Power Rangers (U)

---
+++ Assets/Scripts/SwarmCap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmCap : MonoBehaviour
+{   
+    int counter = 0;
+    swarm swarm_script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other) 
+    {   
+        swarm_script = other.gameObject.GetComponent<swarm>();
+
+        other.gameObject.transform.parent = gameObject.transform;
+
+        if(other.gameObject.tag == "Swarm" && swarm_script.body_part.name == name)
+        {
+            counter++;
+        }
+        if(counter > 30)
+        {
+            gameObject.GetComponent<Collider>().isTrigger = true;
+        }    
+    }
+
+}
--- Assets/Scripts/squad.cs
+++ Assets/Scripts/squad.cs
@@ -43,7 +43,7 @@ public class squad : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(-0.01f,0,0);
+        
     }
     
 }
